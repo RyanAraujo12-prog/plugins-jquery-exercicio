71d58a5854c2fe7a0614da7c8b9f5e4dd6a2d048
RyanAraujo12-prog <ryan.saraiva.araujo32@gmail.com.br>
2025-07-01 18:12:10 -0300

    codigo arrumado

--- js/main.js
+++ js/main.js
@@ -1,42 +1,70 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
-})
+$(document).ready(function(){
+    $('#carousel-imagens').slick({
+        autoplay: true,
+        arrows: false,
+    })
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(DDD) 12345-6789'
+    })
 
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '123.456.789-00'
+    })
 
-$('form').validate({
-    rules: {
-        nome: {
-            requerido: true
+    $('#cep').mask('00000-000', {
+        placeholder: '012345-678'
+    })
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true,
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true,
+            },
+            endereco: {
+                required: true,
+            },
+            cep: {
+                required: true,
+            },
+            cpf: {
+                required: true,
+            },
         },
-        email: {
-            requerido: true,
-            email: true
+        messages: {
+            nome: {
+                required: "Por favor, informe seu nome.",
+            },
+            email: {
+                required: "Por favor, informe seu e-mail.",
+                email: "Por favor, informe um e-mail válido."
+            },
+            telefone: {
+                required: "Por favor, informe seu telefone.",
+            },
+            endereco: {
+                required: "Por favor, informe seu endereço.",
+            },
+            cep: {
+                required: "Por favor, informe seu CEP.",
+            },
+            cpf: {
+                required: "Por favor, informe seu CPF.",
+            },
         },
-        telefone: {
-            requerido: true
+        submitHandler: function (form) {
+            alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+            form.submit();
         },
-        endereco: {
-            requerido: true
-        },
-        cep: {
-            requerido: true
-        },
-        cpf: {
-            requerido: true
-        },
-    },
-    submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
-    },
-    invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
-    }
+        invalidHandler: function (form, validator) {
+            alert("Por favor, preencha os campos para prosseguir com a compra!");
+        }
+    })
 })
